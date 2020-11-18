b59a752c119c44c8efc9382a0baaddb56319413c
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-18 18:58:17 +0000

    added cors dependency

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
